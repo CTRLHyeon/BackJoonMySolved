84ced591c132a3cd9a0b74e3f35eb8a1581b6abe
조정현 <165864420+CTRLHyeon@users.noreply.github.com>
2024-12-03 21:23:31 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -12,25 +12,22 @@ priority_queue<int, vector<int>, greater<int>> heap;
 
 int erased = 0;
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int size = 0;
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int T;
 	cin >> T;
 	while (T--) {
 		int command;
 		cin >> command;
-		if (command) {		//0이 아니면
+		if (command) {
 			heap.push(command);
-			size++;
 		}
-		else {				//0이면
-			if (size == 0) {
+		else {
+			if (heap.size() == 0) {
 				cout << "0\n";
 			}
 			else {
 				cout << heap.top() << '\n';
 				heap.pop();
-				size--;
 			}
 		}
 	}
