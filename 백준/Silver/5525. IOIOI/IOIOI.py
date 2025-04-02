9769c7e48378a29762ea03898f6275334c657011
Jo Jeong Hyeon <165864420+CTRLHyeon@users.noreply.github.com>
2025-04-02 10:24:52 +0900

    [Silver I] Title: IOIOI, Time: 92 ms, Memory: 109544 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -8,13 +8,15 @@ cmp="I"   #IOIOI...
 for i in range(n):
     cmp +="OI"
     
-s = input()
+s = input()     #탐색할 문자열
 
 cnt = 0
 
 for i in range(m-n):
-    if (cmp == s[i:i+2*n+1]):
-        
+    if (s[i]=="O"):
+        continue
+    
+    elif (cmp == s[i:i+2*n+1]):
         cnt += 1
         
 print(cnt)
